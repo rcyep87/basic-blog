ddd4a799cd41e93830352ea409ff28069feafd73
Ryan Yep <ryan.yep@gmail.com>
2015-09-23 08:43:03 -0500

    new_post code...not sure if it'll break everything

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     render template: 'posts/show.html.erb', locals: { post: Post.find(params[:id]), comments: Comment.where(post_id: params[:id]) }
   end
 
+  def new
+    new_form = Post.all.map { |p| [p.title, p.body] }
+    render template: 'posts/new.html.erb', locals: {
+      new_form: Post.new,
+    }
+  end
+
+  def create
+    new_post = Post.new
+    new_post.title = params.fetch(:new_post).fetch(:title)
+    new_post.body  = params.fetch(:new_post).fetch(:body)
+    new_post.published = false
+    if new_post.save
+      redirect_to posts_path(new_post)
+    else
+      redirect_to posts_path
+    end
+  end
+
 end
